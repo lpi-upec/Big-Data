35f5649f0e7ec2493de8fa3b458b845f1b1190bd
edenskull <maxchamp@live.fr>
2019-02-13 17:47:24 +0100

    Add TP 6

---
+++ TP 6/src/main/java/ex6/LicencesLillePerFederation.java
@@ -0,0 +1,29 @@
+package ex6;
+
+import org.apache.log4j.Logger;
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class LicencesLillePerFederation {
+
+	protected static Logger ex6Log = Logger.getLogger(LicencesLillePerFederation.class);
+
+	public static void main(String[] args) {
+		ex6Log.info("ex6.ex6.LicencesLillePerFederation start");
+		Properties properties = new Properties();
+
+		properties.setProperty("fs.default.name", "hdfs://localhost:8020");
+		properties.setProperty("mapred.job.tracker", "http://localhost:10020");
+
+		try {
+			PigServer pigServer = new PigServer(ExecType.MAPREDUCE, properties);
+			InputStream pigStream = LicencesLillePerFederation.class.getClassLoader().getResourceAsStream("licences_lille_per_federation.pig");
+			pigServer.registerScript(pigStream);
+		} catch (Exception e) {
+			ex6Log.error(e.getMessage());
+		}
+	}
+}
